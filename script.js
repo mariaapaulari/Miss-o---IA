ec61eca974b4e98b1e2fdf210a5d21fab69f8366
Maria Paula Espadari <137806233+mariaapaulari@users.noreply.github.com>
2025-06-03 11:24:38 -0300

    a

--- script.js
+++ script.js
@@ -73,11 +73,13 @@ const perguntas = [
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
+
 function mostraPergunta() { 
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
    mostraAlternativas();
 }
+
 function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
     const botaoAlternativas = document.createElement("button");
@@ -85,12 +87,12 @@ function mostraAlternativas() {
     botaoAlternativas.addEventListener("click", () => respostaSelecionada(opcao));
     caixaAlternativas.appendChild(botaoAlternativas);
   }
+}
   function respostaSelecionada(opcaoSelecionada) {
      const afirmacoes = opcaoSelecionada.afirmacoes;
      historiaFinal += afirmacoes + " ";
-  atual++;
+     atual++;
   mostraPergunta();
 }
 
 mostraPergunta();
-}
